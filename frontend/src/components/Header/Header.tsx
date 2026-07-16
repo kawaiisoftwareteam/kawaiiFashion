2dce9c20d559e33796a87950d9fe7670dd0b0bbb
Mirza Hasan <mirzahasanlimon619@gmail.com>
2026-07-16 12:19:17 +0600

    refactor: remove Shop All link and dropdown icon from navigation header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -34,22 +34,6 @@ export default function Header({ cartCount, cartFeedback, onOpenReviews, onOpenC
           <Link href="/" className={styles.navLink}>
             Home
           </Link>
-          <Link href="/#shop" className={styles.navLink}>
-            Shop All
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="12"
-              height="12"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="m6 9 6 6 6-6" />
-            </svg>
-          </Link>
           <Link href="/#new-arrivals" className={styles.navLink}>
             New Arrivals
           </Link>
